5d78150f2d5abcdd7e58e42c3bcb6f7082c25a2f
Bill-GD <geometrydashbill@gmail.com>
2023-03-27 16:41:44 +0700

    Visualized Player Input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './App.css';
 
 const colorDefault = ['White', 'Black', 'Red', 'Yellow', 'Green', 'Blue'];
-let guessHistory = [];
+// let guessHistory = [];
 let chosenColors = [];
 const getPattern = () => {
   chosenColors = [];
@@ -14,9 +14,7 @@ const getPattern = () => {
 const Title = () => {
   return (
     <div className='title'>
-      <p>
-        MASTERMIND GAME
-      </p>
+      <p> MASTERMIND GAME </p>
     </div>
   );
 }
@@ -36,6 +34,10 @@ const ColorRows = ({ guessHistory }) => {
   );
 }
 
+const Pins = ({ guessHistory }) => {
+
+}
+
 const GameBoard = ({ guessHistory }) => {
   return (
     <div className='gameBoard'>
@@ -43,14 +45,14 @@ const GameBoard = ({ guessHistory }) => {
         <ColorRows guessHistory={guessHistory} />
       </div>
       <div className='pinBoard'>
-        Pin Board
+        <Pins guessHistory={guessHistory} />
       </div>
     </div>
   );
 }
 
 
-const Form = ({ handleSubmit }) => {
+const Form = ({ guessHistory, setGuessHistory, currentGuess, setCurrentGuess }) => {
   const handleInputForm = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -61,26 +63,32 @@ const Form = ({ handleSubmit }) => {
       if (formJson[`color${i}`] === '') return;
       array = [...array, formJson[`color${i}`]];
     }
-    guessHistory = [...guessHistory, array];
-    handleSubmit(array);
+    setGuessHistory([...guessHistory, array]);
+  }
+
+  const handleInputChange = (value, name) => {
+    let index = parseInt(name.charAt(name.length - 1)) - 1;
+    let array = [];
+    for (let i = 0; i < currentGuess.length; i++) {
+      if (i === index) array[i] = value;
+      else array[i] = currentGuess[i];
+    }
+    // console.log(array);
+    setCurrentGuess(array);
   }
 
   return (
     <form className='colorInputForm' onSubmit={handleInputForm} method='post' >
-      <select name='color1' key={`color-1`} >
-        {/* onChange={e => handleInput(e.target.value, e.target.name)} > */}
+      <select className='colorSelect' name='color1' key={`color-1`} onChange={e => handleInputChange(e.target.value, e.target.name)} >
         {colorDefault.map(e => (<option value={e}>{e}</option>))}
       </select>
-      <select name='color2' key={`color-2`} >
-        {/* onChange={e => handleInput(e.target.value, e.target.name)} > */}
+      <select className='colorSelect' name='color2' key={`color-2`} onChange={e => handleInputChange(e.target.value, e.target.name)} >
         {colorDefault.map(e => (<option value={e}>{e}</option>))}
       </select>
-      <select name='color3' key={`color-3`} >
-        {/* onChange={e => handleInput(e.target.value, e.target.name)} > */}
+      <select className='colorSelect' name='color3' key={`color-3`} onChange={e => handleInputChange(e.target.value, e.target.name)} >
         {colorDefault.map(e => (<option value={e}>{e}</option>))}
       </select>
-      <select name='color4' key={`color-4`} >
-        {/* onChange={e => handleInput(e.target.value, e.target.name)} > */}
+      <select className='colorSelect' name='color4' key={`color-4`} onChange={e => handleInputChange(e.target.value, e.target.name)} >
         {colorDefault.map(e => (<option value={e}>{e}</option>))}
       </select>
       <button className='submit' type='submit'>Submit</button>
@@ -88,42 +96,43 @@ const Form = ({ handleSubmit }) => {
   )
 }
 
-const PlayerInput = ({ handleSubmit }) => {
+const PlayerInput = ({ guessHistory, setGuessHistory, currentGuess, setCurrentGuess }) => {
   return (
     <div className='playerInput'>
-      <div className='colorInput'>
-        <Form handleSubmit={handleSubmit} />
+      <div className='colorRow'>
+        {currentGuess.map((color) => (
+          <div className={`colorCircle ${color}`} ></div>
+        ))}
       </div>
+      <Form guessHistory={guessHistory} setGuessHistory={setGuessHistory} currentGuess={currentGuess} setCurrentGuess={setCurrentGuess} />
     </div>
   );
 }
 
-const Board = ({ guessHistory, handleSubmit }) => {
+const Board = ({ guessHistory, setGuessHistory, currentGuess, setCurrentGuess }) => {
   return (
     <div className='board'>
       <Title />
       <GameBoard guessHistory={guessHistory} />
-      <PlayerInput handleSubmit={handleSubmit} />
+      <PlayerInput guessHistory={guessHistory} setGuessHistory={setGuessHistory} currentGuess={currentGuess} setCurrentGuess={setCurrentGuess} />
     </div>
   );
 }
 
+let gameOver = true;
+
 const Game = ({ colorList }) => {
   // state of component only change if <setState> function is used
-  const [currentGuess, setCurrentGuess] = useState([]);
-  const handleSubmit = (current) => {
-    if (guessHistory.length > 10) return;
-    setCurrentGuess(current);
-    // /\ used here, if change state directly -> doesn't work
-    // \/ but when use setState, it 1 state behind
-    console.log(currentGuess);
-  }
-  let gameOver = true;
+  const [currentGuess, setCurrentGuess] = useState(Array(4).fill('White'));
+  const [guessHistory, setGuessHistory] = useState([]);
 
   const buttonRestart_Click = () => {
     getPattern();
-    guessHistory = [];
-    setCurrentGuess([]);
+    setGuessHistory([]);
+    setCurrentGuess(Array(4).fill('White'));
+    // gameOver = !gameOver;
+    for (let element of document.getElementsByClassName('colorSelect'))
+      element.value = 'White';
   };
 
   return (
@@ -137,16 +146,14 @@ const Game = ({ colorList }) => {
                 Restart
               </button>
             </>)
-          :
-          (<></>)
+          : (<></>)
         }
       </div>
-      <Board guessHistory={guessHistory} handleSubmit={handleSubmit} />
+      <Board guessHistory={guessHistory} setGuessHistory={setGuessHistory} currentGuess={currentGuess} setCurrentGuess={setCurrentGuess} />
       <div className='timer'>
         {gameOver ?
           (<><p>Time: 00:12:43</p></>)
-          :
-          (<></>)
+          : (<></>)
         }
       </div>
     </div>
